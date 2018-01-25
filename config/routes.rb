d6590e191150a600be4e687e68483aa58b5992d3
muranto <kmsask0908@gmail.com>
2018-01-25 11:12:43 +0900

    try

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home/top'
+  root 'home#top'
   get "home/movie" => "home#movie"
   get "home/about" =>"home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
